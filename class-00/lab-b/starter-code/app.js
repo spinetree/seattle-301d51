7f5a541e3ea8d5e0604338b5a4dccbeadd2fc931
spinetree <steve@spinetree.org>
2019-09-09 11:34:48 -0700

    lab 00b done

--- class-00/lab-b/starter-code/app.js
+++ class-00/lab-b/starter-code/app.js
@@ -160,7 +160,13 @@ const message = name => `Hello, ${name}!`;
 console.log(message('Allie'));
 
 
-let Student = function(name, age, hometown) {
+// let Student = function(name, age, hometown) {
+//   this.name = name;
+//   this.age = age;
+//   this.hometown = hometown;
+// };
+
+const Student = function(name, age, hometown) {
   this.name = name;
   this.age = age;
   this.hometown = hometown;
@@ -170,7 +176,7 @@ let joe = new Student('Joe', 'Schmoe', 100);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this code to break!
-// console.log(joe);
+console.log(joe);
 
 // TODO: After viewing the previous console.log(), return the code to a working state.
 
@@ -180,20 +186,26 @@ Student.prototype.greeting = function() {
   return `Hi, my name is ${this.name}`;
 };
 
+// breaks:
+// Student.prototype.greeting = () => {
+//   return `Hi, my name is really ${this.name}`;
+// };
+
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this method to break!
-// console.log(joe.greeting());
+console.log(joe.greeting());
 
 // TODO: After viewing the previous console.log(), return the code to a working state.
 
 
+// Student.courseName = function() {
+//   return 'This student is enrolled in Code 301.';
+// };
 
-Student.courseName = function() {
-  return 'This student is enrolled in Code 301.';
-};
+Student.courseName = () => 'This student is enrolled in Code 301.';
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(Student.courseName());
+console.log(Student.courseName());
 
 
 
@@ -204,17 +216,17 @@ Student.prototype.scope = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scope());
+console.log(joe.scope());
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scopeArrow());
+console.log(joe.scopeArrow());
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
-// 2. What is "this" when joe.scopeArrow() is invoked?
-//
+// the joe instance of the student object
+// 2. What is "this" when joe.scopeArrow() is invoked
+//  undefined - it's the this of its parent object
 // 3. Explain why "this" is different when an arrow function is used.
-//
+//  The arrow notation is scoped differently and doesn't have its own this object.
