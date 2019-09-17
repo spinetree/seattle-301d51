065737bd0b00a090d92d798d35ed3d61c441ee6a
spinetree <steve@spinetree.org>
2019-09-17 09:43:18 -0700

    demo notes for class6 messy tbh

---
+++ class-06/demo/Notes.js
@@ -0,0 +1,6 @@
+
+// method for including a local file (via node, require belongs to node)
+const geoDataresults = require('./data.geo.json); 
+
+// and now we can point at that via geoDataresults.[0].location.lat etc
+
